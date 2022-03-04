1b22d7a4a63017d340f4fb68ccc6d253e4b93050
christopfer cardoso <cristofer.3c@hotmail.com>
2022-03-04 14:38:36 -0300

    Correciones de NavBar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -20,8 +20,15 @@ const NavBar = () => {
                             <li className="nav-item">
                                 <a className="nav-link active" href="#">Novedades</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#">Contacto</a>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Contacto
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="#">Facebook</a></li>
+                                    <li><a className="dropdown-item" href="#">Instagram</a></li>
+                                    <li><a className="dropdown-item" href="#">Whatsapp</a></li>
+                                </ul>
                             </li>
                         </ul>
                     </div>
