3dbca89bbe6a31de3d3752122ca0748d0489c875
christopfer cardoso <cristofer.3c@hotmail.com>
2022-03-04 14:04:10 -0300

    realización de NavBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrouserRouter as Router, Switch, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Santisublime
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <router>
+        <NavBar/>
+      </router>
     </div>
   );
 }
