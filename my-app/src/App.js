5f200a38ee36133275e4544c75a85a97d8748993
christopfer cardoso <cristofer.3c@hotmail.com>
2022-03-10 19:34:54 -0300

    instalación de dependencias

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {BrouserRouter as Router, Switch, Route} from 'react-router-dom'
 import NavBar from './components/NavBar'
 import CartWidget from './components/CartWidget'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const ItemListContainer = () =>{
   return <div><h2>Le damos la bienvenida</h2></div>
